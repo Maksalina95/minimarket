24b7171171fcb4f27da0b411ab396fb2862941f1
Maksalina95 <maksalinamagazin@gmail.com>
2025-08-08 18:12:12 +0300

    Create contacts.js

---
+++ scripts/contacts.js
@@ -0,0 +1,9 @@
+export function showContacts(container) {
+  container.innerHTML = `
+    <h2>📱 Контакты</h2>
+    <p>
+      <a href="https://wa.me/79376280080" target="_blank">89376280080</a><br>
+      <a href="https://wa.me/79649626694" target="_blank">89649626694</a>
+    </p>
+  `;
+}
