ccebd2097a87d4316d106a89846635b0649508b9
Maksalina95 <maksalinamagazin@gmail.com>
2025-08-08 18:11:52 +0300

    Create delivery.js

---
+++ scripts/delivery.js
@@ -0,0 +1,7 @@
+// delivery.js
+export async function showDelivery(container) {
+  container.innerHTML = `
+    <h2>Доставка</h2>
+    <p>Здесь будет информация о способах, сроках и стоимости доставки.</p>
+  `;
+}
