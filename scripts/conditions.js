49c696d4324562dd4add12581379d90c3b3a9c4d
Maksalina95 <maksalinamagazin@gmail.com>
2025-08-08 18:11:24 +0300

    Create conditions.js

---
+++ scripts/conditions.js
@@ -0,0 +1,6 @@
+export async function showConditions(container) {
+  container.innerHTML = `
+    <h2>Условия возврата и гарантии</h2>
+    <p>Здесь будет информация о возврате, гарантии, обмене и т.п.</p>
+  `;
+}
